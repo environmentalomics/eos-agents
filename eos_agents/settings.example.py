e806dd826985e088a35bc9778e8a5c31991e36ac
Ben Collier <bmcollier@gmail.com>
2015-03-26 10:04:40 +0000

    Example settings file.

---
+++ eos_agents/settings.example.py
@@ -0,0 +1,11 @@
+
+class VCDetails():
+    username = 'username'  # Username
+    org = 'eoscloud-u'  # Organisation
+    password = 'password'  # Password
+    endpoint = 'https://vcloud.ceda.ac.uk:443/api/'  # for CEH
+
+class DBDetails():
+    username = 'notused'  # ???????
+    password = 'notused'  # ?????
+
