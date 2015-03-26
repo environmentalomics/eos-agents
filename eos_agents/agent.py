18eae9705bceefa3c3215336debf4a4ab05efe11
Tim Booth <tbooth@ceh.ac.uk>
2015-03-26 10:05:59 +0000

    Fix to work on Py3

--- eos_agents/agent.py
+++ eos_agents/agent.py
@@ -3,14 +3,14 @@ from time import sleep
 
 def set_state_to_boosting(self, vm_id):
     """
-    
+
     """
     session2 = db_client.DBSession(self.shared_username, self.shared_password)
     session2.set_state_to_boosting(vm_id)
 
 def set_state_to_deboosting(self, vm_id):
     """
-    
+
     """
     session2 = db_client.DBSession(self.shared_username, self.shared_password)
     session2.set_state_to_deboosting(vm_id)
@@ -19,7 +19,7 @@ def boost_vm_memory(self, vm_id):
     session2 = db_client.DBSession(self.shared_username, self.shared_password)
     cores, ram = session2.get_latest_specification(vm_id)
     actions.boost_vm_memory(vm_id, ram)
-    
+
 def boost_vm_cores(self, vm_id):
     session2 = db_client.DBSession(self.shared_username, self.shared_password)
     cores, ram = session2.get_latest_specification(vm_id)
@@ -28,76 +28,76 @@ def boost_vm_cores(self, vm_id):
 class Agent():
     """
     An agent performs activities within the system as follows:
-    
+
     1.    Regularly polls the DB API to check for artifacts which are in a
           state which requires their action.
     2.    If this is the case, carries out the series of actions as defined
           by the agent's class.
     3.    Having performed this series of actions, updates the API to a new
           state.
-          
+
     As such, each agent should have a trigger state, and a list of functions to
     perform on the artifact identified.
-    
+
     """
     def __init__(self, trigger_state, action_list, success_state, error_state):
         self.trigger_state = trigger_state
         self.action_list = action_list
         self.success_state = success_state
         self.error_state = error_state
-        
+
         self.shared_username = 'agent'
         self.shared_password = 'asdf'
-        
-    
+
+
     def wait_on_job(self, job_id):
         status = ""
         while status not in ['success', 'error', 'canceled', 'aborted']:
             status = actions.get_status(job_id)
         return status
-    
+
     def dwell(self):
         while True:
-            
+
             # Open DB Connection
             session = db_client.DBSession(self.shared_username, self.shared_password)
-            
+
             # Look for Machines in target state
             vm_id, uuid = session.get_machine_in_state(self.trigger_state)
-                           
+
             if vm_id != None:
                 serveruuid = self.lookup_uuid(vm_id)
                 if serveruuid != None:
-                    print "Found action for server " + vm_id
+                    print("Found action for server " + vm_id)
                     for job in self.action_list:
-                        
+
                         status_code, job_id = job(serveruuid) # Execute VM action
-                        print "Waiting for response"
+                        print("Waiting for response")
                         status = self.wait_on_job(job_id) # Wait for job to complete
-                        
+
                         if status == "success":
-                            print "Success"
+                            print("Success")
                             session.set_state(vm_id, self.success_state)    # Perform success action
                         elif status == "error":
-                            print "Error"        # Flag machine as "alert" and return to previous state
+                            print("Error")        # Flag machine as "alert" and return to previous state
                             session.set_state(vm_id, self.error_state)
                         elif status == "canceled":
-                            print "Cancelled"    # Return machine state to previous state
+                            print("Cancelled")    # Return machine state to previous state
                             session.set_state(vm_id, self.error_state)
                         elif status == "aborted":
-                            print "Aborted"      # Return machine state to previous state
+                            print("Aborted")      # Return machine state to previous state
                             session.set_state(vm_id, self.error_state)
                         else:
-                            print "Error: Status=" + str(status)
+                            print("Error: Status=" + str(status))
             sleep(5)
-    
+
 """
 
 Start : Working
 Stop: Working
 Restart: Not Working
 Prepare: -
-Pre-Deboost: -  
+Pre-Deboost: -
 
 prepare_agent = Agent("Preparing", [actions.stop_vm], "Prepared", "Started")
 predeboost_agent = Agent("Pre_Deboosting", [actions.stop_vm], "Pre_Deboosted", "Started")
